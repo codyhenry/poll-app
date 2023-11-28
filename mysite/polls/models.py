8eb9f2aad8070f1bf0751cd9d59df050323c76af
codyhenry <codyhenry527@gmail.com>
2023-11-28 16:51:35 -0500

    added db tables

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,3 +9,10 @@ class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
+
+
+"""
+Change models in models.py file
+Run <py manage.py makemigrations> to create migrations for the changes
+Run <py manage.py migrate> to apply the changes to the database
+"""
