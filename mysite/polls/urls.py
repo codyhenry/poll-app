f0e187fce88beac956ed677c0f1b76cf34bb35e1
codyhenry <codyhenry527@gmail.com>
2023-11-29 18:24:07 -0500

    added generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 # namespace for urls
 app_name = "polls"
+""" old views not using generic views
 urlpatterns = [
   #/polls
   path("", views.index, name="index"),
@@ -11,6 +12,19 @@ urlpatterns = [
   path("<int:question_id>/", views.detail, name="detail"),
   #/polls/5/results
   path("<int:question_id>/results", views.results, name="results"),
+  #/polls/5/vote
+  path("<int:question_id>/vote", views.vote, name="vote"),
+]
+"""
+
+#int:pk used because DetailView expects primary key value to be called "pk"
+urlpatterns = [
+  #/polls
+  path("", views.IndexView.as_view(), name="index"),
+  #/polls/5/
+  path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+  #/polls/5/results
+  path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
   #/polls/5/vote
   path("<int:question_id>/vote", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 # Create your views here.
@@ -15,11 +16,13 @@ def index(request):
   return HttpResponse(template.render(context,request))
 """
 
+"""old endpoint
 #shortcut to load template
 def index(request):
   latest_question_list = Question.objects.order_by("-pub_date")[:5]
   context = {"latest_question_list": latest_question_list}
   return render(request, "polls/index.html", context)
+"""
 
 """
 def detail(request,question_id):
@@ -31,6 +34,7 @@ def detail(request,question_id):
   # return HttpResponse("You're looking at question %s." % question_id) 
 """
 
+""" old endpoints
 #substitute try except with get_object_or_404
 def detail(request,question_id):
   question = get_object_or_404(Question, pk=question_id)
@@ -40,7 +44,29 @@ def detail(request,question_id):
 def results(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, "polls/results.html", {"question": question})
+"""
 
+class IndexView(generic.ListView):
+  #will default to <app name>/<model name>_list.html if not given template_name
+  #ex. polls/question_list.html
+  template_name = "polls/index.html"
+  #will default to <model name>_list if not given context_object_name
+  #ex. question_list
+  context_object_name = "latest_question_list"
+
+  def get_queryset(self):
+    return Question.objects.order_by("-pub_date")[:5]
+  
+class DetailView(generic.DetailView):
+  model = Question
+  #will default to <app name>/<model name>_detail.html if not given template_name
+  #ex. polls/question_detail.html
+  template_name = "polls/detail.html"
+  #context_object_name will default to question
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
